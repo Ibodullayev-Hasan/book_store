82009ed1dcda84e092e621fac819229f62fcfa6c
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2025-01-18 23:17:45 +0500

    kod optimizatsiyasi

--- src/public/javaScript/homeScript.js
+++ src/public/javaScript/homeScript.js
@@ -1,18 +1,22 @@
 const header = document.getElementById("header");
+let d = 1;
 
 const intervalTime = setInterval(() => {
-  let d = 0;
-  d++;
-  if (new Date().getSeconds() == 0) {
-    if (d < 4) {
-      console.log(d);
+  let time = new Date().getSeconds();
+
+  // console.log(`time:${time}`);
+  if (time === 0) {
+    d++;
+    if (d <= 5) {
+      // console.log(`D: ${d}`);
       header.style.transition = `all .8s`;
       header.style.backgroundImage = `url("/images/k${d}.jpg")`;
       header.style.backgroundPosition = "center";
       header.style.backgroundRepeat = "no-repeat";
       header.style.backgroundSize = "cover";
     } else {
-      d -= 4;
+      d -= 5;
+      // console.log(`Dy: ${d}`);
     }
   }
 }, 1000);
