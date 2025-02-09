efe5ca92f6bc68eb1200e568887ae186155a9dc1
Ibodullayev_Hasan <ibodullayevh040@gmail.com>
2025-02-09 14:40:16 +0500

    dotenv add, vercel.json add and optimision code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 let cookieParser = require("cookie-parser");
 const path = require("path");
 const router = require("./src/Routes/indexRout");
+require("dotenv/config");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.static(path.join(process.cwd(), "src", "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(9000, () => {
-  console.log(`Server run 🚀 `);
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server run 🚀 on port: ${port} `);
 });
